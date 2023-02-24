bb773f229381e5934aa6942c01a82d7e6efc0f63
Remberto1238 <125625467+Remberto1238@users.noreply.github.com>
2023-02-23 22:04:49 -0600

    Add files via upload

---
+++ Actividad #5/Bucle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Proyec 
+{
+    class Proyec
+    {
+        static void Main(string[] args)
+        {
+            int resultado, numero, numero1;
+            Console.WriteLine("ingrese el primer rango de numero ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo rango de numero");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = numero; i <= numero1; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
